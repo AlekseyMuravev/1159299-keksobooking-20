70e722bc7e095357096b0aac98668b86353129a2
Алексей Муравьёв <chacnoriss@gmail.com>
2020-06-22 20:01:41 +0300

    пока ничего

--- js/map.js
+++ js/map.js
@@ -90,7 +90,6 @@
     });
   };
 
-
   // откртыие попапа Enter
   var openPopupKey = function (element, index) {
     element.addEventListener('keydown', function (evt) {
@@ -116,9 +115,50 @@
   }
 
   var address = document.querySelector('input[name=address]');
-  var pinTop = parseInt(mapPinMain.style.top, 10) + window.data.HEIGHT_MAP_PIN + 22;
-  var pinLeft = Math.round(parseInt(mapPinMain.style.left, 10) + window.data.WIDTH_MAP_PIN / 2);
-  var addressFromPin = pinLeft + 'px' + ', ' + pinTop + 'px';
+  var pinMainTop = parseInt(mapPinMain.style.top, 10) + window.data.HEIGHT_MAP_PIN + 22;
+  var pinMainLeft = Math.round(parseInt(mapPinMain.style.left, 10) + window.data.WIDTH_MAP_PIN / 2);
+  var addressFromPin = pinMainLeft + 'px' + ', ' + pinMainTop + 'px';
 
   address.value = addressFromPin;
+
+  mapPinMain.addEventListener('mousedown', function (evt) {
+    if (evt.which === 1) {
+      evt.preventDefault();
+      var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY,
+      }
+
+      console.log(startCoords)
+
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+
+        var shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY,
+        }
+
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY,
+        }
+
+        mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+        mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+        console.log(mapPinMain.style.left)
+      };
+
+      var onMouseUp = function (moveEvt) {
+        moveEvt.preventDefault();
+
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      }
+    }
+
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  })
 })();
